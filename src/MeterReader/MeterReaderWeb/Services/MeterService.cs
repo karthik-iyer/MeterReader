4873be696da6438f27ca1f5e46ab547dee21827b
Karthik.Iyer <Karthik.Iyer@CLK-6VLWZY2>
2020-11-24 00:04:35 -0600

    gRPC project

---
+++ src/MeterReader/MeterReaderWeb/Services/MeterService.cs
@@ -0,0 +1,63 @@
+﻿using Grpc.Core;
+using MeterReaderWeb.Data;
+using MeterReaderWeb.Data.Entities;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeterReaderWeb.Services
+{
+    public class MeterService : MeterReadingService.MeterReadingServiceBase
+    {
+        private readonly ILogger<MeterService> _logger;
+        private readonly IReadingRepository _repository;
+
+        public MeterService(ILogger<MeterService> logger, IReadingRepository readingRepository)
+        {
+            _logger = logger;
+            _repository = readingRepository;
+
+        }
+        public override async Task<StatusMessage> AddReading(ReadingPacket request, ServerCallContext context)
+        {
+            var result = new StatusMessage
+            {
+                Success = ReadingStatus.Failure
+            };
+
+            if(request.Successful == ReadingStatus.Success)
+            {
+                try
+                {
+                    foreach(var r in request.Readings)
+                    {
+                        //Save it to DB
+                        var reading = new MeterReading(){
+                           CustomerId = r.CustomerId,
+                           ReadingDate = r.ReadingTime.ToDateTime(),
+                           Value = r.ReadingValue
+                        };
+                        _repository.AddEntity(reading);
+                        
+                    }
+
+                    if(await _repository.SaveAllAsync())
+                    {
+                        _logger.LogInformation($"Stored {request.Readings.Count} New Readings...");
+                        result.Success = ReadingStatus.Success;
+                    }
+
+                }
+                catch(Exception ex)
+                {
+                    result.Message = "Exception thrown when trying to save to Database";
+                    _logger.LogError($"Execption thrown during saving of the readings: {ex}");
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/MeterReader/PythonClient/PythonClient.py
@@ -0,0 +1,10 @@
+import grpc
+import MeterReader_pb2 as MeterReader
+import MeterReader_pb2_grpc as MeterReaderService
+
+
+def main():
+    print("Calling gRPC Service")
+
+
+main()
